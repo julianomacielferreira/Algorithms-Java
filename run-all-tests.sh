b9f87c7ef61b42a97b453235463f0194d3a90219
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2020-04-28 09:39:06 -0300

    Add cli scripts.

--- run-all-tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-javac -cp "lib/algs4.jar:lib/junit-4.13.jar:dist/algorithms/fundamentals/*" -sourcepath "test/algorithms" -d "dist" -g test/algorithms/*/*.java
-#cd dist/
-#java -cp "../lib/algs4.jar:../lib/junit-4.13.jar" org.junit.runner.JUnitCore algorithms/* test/algorithms/MatrixTest
